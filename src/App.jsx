2b74b6a6aaf3b0ece423ccc55dce1406c3c682ed
AyoubSasi20 <ayoub.eswie@outlook.com>
2026-03-08 18:41:47 +0200

    apply feedback for better semantic and a11y

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,38 @@ import cart from "./assets/images/icon-cart.svg";
 const App = () => {
   return (
     <>
-      <div className="card">
+      <article className="card">
         <picture className="perfum">
           <source media="(max-width: 767px)" srcSet={imgM} />
           <img src={imgD} alt="Gabrielle Essence Eau De Parfum" />
         </picture>
         <div className="content">
-          <p className="category">PERFUME</p>
+          <p className="category" aria-label="Product Category">
+            PERFUME
+          </p>
           <h1> Gabrielle Essence Eau De Parfum</h1>
           <p className="discription">
             A floral, solar and voluptuous interpretation composed by Olivier
             Polge, Perfumer-Creator for the House of CHANEL.
           </p>
           <div className="prices">
-            <p className="current-price">$149.99</p>
-            <s>$169.99</s>
+            <p
+              className="current-price"
+              aria-label="Current price: 149 dollars and 99 cents"
+            >
+              $149.99
+            </p>
+            <del aria-label="Original price: 169 dollars and 99 cents">
+              $169.99
+            </del>
           </div>
           <div className="cart-btn">
             <button className="btn">
-              <img src={cart} /> Add to Cart
+              <img src={cart} alt="cart icon" /> Add to Cart
             </button>
           </div>
         </div>
-      </div>
+      </article>
       <div className="attribution">
         <p>
           Challenge by{" "}
